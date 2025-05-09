9f6e839fdf73c494cf771413307c6377884270a5
vainiovesa <vesa.vainio@helsinki.fi>
2025-05-09 19:38:43 +0300

    Add feedforwarding

--- activation_functions.py
+++ activation_functions.py
@@ -1,2 +1,7 @@
+from math import exp
+
 def relu(x):
     return x if x > 0 else 0
+
+def sigmoid(x):
+    return 1 / (1 + exp(-x))
--- network.py
+++ network.py
@@ -6,6 +6,12 @@ class Network:
         for i in range(1, len(size)):
             self.layers.append(Layer(size[i], size[i - 1], activation_functions[i - 1]))
 
+    def feedforward(self, inputs:list):
+        last = inputs
+        for layer in self.layers:
+            last = layer.activations(last)
+        return last
+
     def __repr__(self):
         return f"Network {self.layers}"
 
@@ -14,6 +20,13 @@ class Layer:
         self.neurons = []
         for _ in range(size):
             self.neurons.append(Neuron(previous_layer_size, activation_function))
+        self.previous_activations = None
+
+    def activations(self, previous_layer_outputs:list):
+        self.previous_activations = []
+        for neuron in self.neurons:
+            self.previous_activations.append(neuron.activation(previous_layer_outputs))
+        return self.previous_activations
 
     def __repr__(self):
         return f"Layer: {self.neurons}"
