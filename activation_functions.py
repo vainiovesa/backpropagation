a68e821418620a1cdca5ea705e1dc77a01bbaa52
vainiovesa <vesa.vainio@helsinki.fi>
2025-05-10 12:05:20 +0300

    Add derivatives

--- activation_functions.py
+++ activation_functions.py
@@ -3,5 +3,11 @@ from math import exp
 def relu(x):
     return x if x > 0 else 0
 
+def relu_derivative(x):
+    return 1 if x > 0 else 0
+
 def sigmoid(x):
     return 1 / (1 + exp(-x))
+
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
