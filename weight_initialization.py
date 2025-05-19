2dfcd772b1c593c919a80d706ad259a0dad090c3
vainiovesa <vesa.vainio@helsinki.fi>
2025-05-19 20:43:59 +0300

    Add Kaiming initialization

--- weight_initialization.py
+++ weight_initialization.py
@@ -1,5 +1,5 @@
 from math import sqrt
-from random import random, uniform
+from random import random, uniform, normalvariate
 from network import Layer
 
 def basic(layer:Layer):
@@ -8,9 +8,18 @@ def basic(layer:Layer):
         neuron.set_weights_and_bias(new, 0)
 
 def glorot(layer:Layer):
+    """To be used with sigmoid and tanh"""
     fan_in = len(layer.neurons[0].weights)
     fan_out = len(layer.neurons)
     boundary = sqrt(6) / sqrt(fan_in + fan_out)
     for neuron in layer.neurons:
         new = [uniform(-boundary, boundary) for _ in range(len(neuron.weights))]
+        neuron.set_weights_and_bias(new, 0)
+
+def kaiming(layer:Layer):
+    """To be used with ReLU"""
+    n = len(layer.neurons[0].weights)
+    deviation = sqrt(2 / n)
+    for neuron in layer.neurons:
+        new = [abs(normalvariate(sigma=deviation)) for _ in range(len(neuron.weights))]
         neuron.set_weights_and_bias(new, 0)
